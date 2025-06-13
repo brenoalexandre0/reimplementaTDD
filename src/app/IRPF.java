df3474f071662639025ed5fbf6237d40b4598bce
Breno Alexandre <i000090000@gmail.com>
2025-06-12 23:02:16 -0300

    upd

---
+++ src/app/IRPF.java
@@ -0,0 +1,54 @@
+package app;
+
+public class IRPF {
+
+    private float salario;
+    private float contribuicaoPrevidenciaria;
+    private String[] dependentes;
+
+    public IRPF() {
+        dependentes = new String[0];
+    }
+
+    public void cadastrarSalario(float salario) {
+        this.salario += salario;
+    }
+
+    public float getTotalSalario() {
+        return this.salario;
+    }
+
+    public void cadastrarPrevidenciaOficial(float contribuicaoPrevidenciaria) {
+        this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
+    }
+
+    public float getPrevidenciaOficial() {
+        return this.contribuicaoPrevidenciaria;
+    }
+
+    public void cadastrarDependente(String nome) {
+        // Criar uma nova posição no vetor de dependentes
+        String[] temp = new String[dependentes.length + 1];
+        for (int i = 0; i < dependentes.length; i++) {
+            temp[i] = dependentes[i];
+        }
+
+        // Alocar novo dependente no vetor
+        temp[dependentes.length] = nome;
+
+        // Atualizar referências do vetor de dependentes
+        dependentes = temp;
+    }
+
+    public float getDeducaoDependentes() {
+        return dependentes.length * 189.59f;
+//
+//        if (dependentes.length == 1)
+//            return 189.59f; // pela técnica de falsificação
+//        else if (dependentes.length > 1)
+//            return 379.18f; // pela técnica de falsificação / duplicação
+//        else
+//            return 0;
+    }
+
+}
